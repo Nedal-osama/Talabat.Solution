d558ee796d4bba60e8747aa482590872b31b0c6f
Nedal <nedalosama3@gmail.com>
2024-09-30 15:25:19 +0300

    Add project files.

---
+++ Talabat.Core/Entites/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entites
+{
+	public class Product:BaseEntity
+	{
+        public string Name { get; set; }
+		public string Description { get; set; }
+		public string PictureUrl { get; set; }
+		public decimal Price { get; set;}
+		public int BrandId { get; set;}//Fk
+		public ProductBrand Brand { get; set; }
+        public int caregoryId { get; set; }//Fk
+        public ProductCategory Category { get; set; }
+    }
+}
